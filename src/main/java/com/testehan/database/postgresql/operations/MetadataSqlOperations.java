bc2282d7839f07f491adf44ed182362a8ec9c44d
DanTestehan <tdan89@yahoo.com>
2022-10-16 19:14:03 +0300

    added method that prints the available types of ResultSets for the postgre driver

--- src/main/java/com/testehan/database/postgresql/operations/MetadataSqlOperations.java
+++ src/main/java/com/testehan/database/postgresql/operations/MetadataSqlOperations.java
@@ -24,4 +24,62 @@ public class MetadataSqlOperations extends SqlOperationsBase{
             throw new RuntimeException(e);
         }
     }
+
+    public void printSupportedResultSetTypes(){
+        try (Connection connection = connectionPool.getConnection()) {
+            DatabaseMetaData dbmd = connection.getMetaData();
+
+            boolean isForwardOnlySupported = dbmd.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY);
+            if(isForwardOnlySupported) {
+                System.out.println("Underlying database supports TYPE_FORWARD_ONLY ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports TYPE_FORWARD_ONLY ResultSet type");
+            }
+
+            boolean isScrollInsensitiveSupported = dbmd.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE);
+            if(isScrollInsensitiveSupported) {
+                System.out.println("Underlying database supports TYPE_SCROLL_INSENSITIVE ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports TYPE_SCROLL_INSENSITIVE ResultSet type");
+            }
+
+            boolean isScrollSensitiveSupported = dbmd.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE);
+            if(isScrollSensitiveSupported) {
+                System.out.println("Underlying database supports TYPE_SCROLL_INSENSITIVE ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports TYPE_SCROLL_INSENSITIVE ResultSet type");
+            }
+
+            boolean isConcurrentReadOnlySupported = dbmd.supportsResultSetType(ResultSet.CONCUR_READ_ONLY);
+            if(isConcurrentReadOnlySupported) {
+                System.out.println("Underlying database supports CONCUR_READ_ONLY ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports CONCUR_READ_ONLY ResultSet type");
+            }
+
+            boolean isConcurrentUpdatableSupported = dbmd.supportsResultSetType(ResultSet.CONCUR_UPDATABLE);
+            if(isConcurrentUpdatableSupported) {
+                System.out.println("Underlying database supports CONCUR_UPDATABLE ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports CONCUR_UPDATABLE ResultSet type");
+            }
+
+            boolean isHoldCursorsOverCommitSupported = dbmd.supportsResultSetType(ResultSet.HOLD_CURSORS_OVER_COMMIT);
+            if(isHoldCursorsOverCommitSupported) {
+                System.out.println("Underlying database supports HOLD_CURSORS_OVER_COMMIT ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports HOLD_CURSORS_OVER_COMMIT ResultSet type");
+            }
+
+            boolean isCloseCursorsAtCommitSupported = dbmd.supportsResultSetType(ResultSet.CLOSE_CURSORS_AT_COMMIT);
+            if(isCloseCursorsAtCommitSupported) {
+                System.out.println("Underlying database supports CLOSE_CURSORS_AT_COMMIT ResultSet type");
+            } else {
+                System.out.println("Underlying database does not supports CLOSE_CURSORS_AT_COMMIT ResultSet type");
+            }
+
+        }  catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
